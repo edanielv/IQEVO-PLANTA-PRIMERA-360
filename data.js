c4585dbec135cd0de674d6389115e00c8fe34ce1
edanielv <edanielvaron.arq@gmail.com>
2025-11-03 00:21:19 -0500

    imgs

--- data.js
+++ data.js
@@ -1,51 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-360-saln",
-      "name": "360-SALÓN",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.7905543744097479,
-          "pitch": 0.17434269294316884,
-          "rotation": 0.7853981633974483,
-          "target": "1-360-recibidor"
-        },
-        {
-          "yaw": -1.9157569590301975,
-          "pitch": 0.39453221613736744,
-          "rotation": 1.5707963267948966,
-          "target": "5-360-dormitorio-2"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-360-recibidor",
+      "id": "0-360-recibidor",
       "name": "360-RECIBIDOR",
       "levels": [
         {
@@ -74,22 +30,66 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.029448159675313335,
-          "pitch": 0.30788164530696704,
-          "rotation": 0,
-          "target": "0-360-saln"
+          "yaw": 0.014727378836390415,
+          "pitch": 0.23140663794130845,
+          "rotation": 5.497787143782138,
+          "target": "1-360-saln"
         },
         {
-          "yaw": -2.669030314807248,
-          "pitch": 0.31978059867217823,
-          "rotation": 0.7853981633974483,
-          "target": "2-360_bao-cortesa"
-        },
-        {
-          "yaw": 3.115083165100784,
-          "pitch": 0.22815410587453933,
+          "yaw": 3.127159067487873,
+          "pitch": 0.16174871334243335,
           "rotation": 0,
           "target": "3-360_dormitorio-1"
+        },
+        {
+          "yaw": -2.637464527641665,
+          "pitch": 0.22798877975678877,
+          "rotation": 0.7853981633974483,
+          "target": "2-360_bao-cortesa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360-saln",
+      "name": "360-SALÓN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8161083459028937,
+          "pitch": 0.1423064594442227,
+          "rotation": 0.7853981633974483,
+          "target": "0-360-recibidor"
+        },
+        {
+          "yaw": -1.919706203399091,
+          "pitch": 0.3355962593868149,
+          "rotation": 1.5707963267948966,
+          "target": "5-360-dormitorio-2"
         }
       ],
       "infoHotspots": []
@@ -118,16 +118,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.32708174705542703,
-        "pitch": 0.1929276545773888,
+        "yaw": -0.256456825969984,
+        "pitch": 0.31788362954095994,
         "fov": 1.325599857056214
       },
       "linkHotspots": [
         {
-          "yaw": -1.5829693317135458,
-          "pitch": 0.3497812605076476,
+          "yaw": -1.6562679442849806,
+          "pitch": 0.2831503702791327,
           "rotation": 0,
-          "target": "1-360-recibidor"
+          "target": "0-360-recibidor"
         }
       ],
       "infoHotspots": []
@@ -156,22 +156,22 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.00376771080054894,
+        "pitch": 0.28186837864387293,
+        "fov": 1.325599857056214
       },
       "linkHotspots": [
         {
-          "yaw": 2.1384588802956177,
-          "pitch": 0.26232194799473874,
+          "yaw": 2.1414373236378488,
+          "pitch": 0.1995951535684508,
           "rotation": 0,
           "target": "4-360-bao-principal"
         },
         {
-          "yaw": 2.385002401371965,
-          "pitch": 0.20939172064154477,
+          "yaw": 2.3948065986996134,
+          "pitch": 0.1814521544445249,
           "rotation": 0.7853981633974483,
-          "target": "1-360-recibidor"
+          "target": "0-360-recibidor"
         }
       ],
       "infoHotspots": []
@@ -200,14 +200,14 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.0872948524456767,
-        "pitch": 0.2452760789434496,
+        "yaw": -0.007123792865286305,
+        "pitch": 0.23778696697947055,
         "fov": 1.325599857056214
       },
       "linkHotspots": [
         {
-          "yaw": 0.0004199883640971791,
-          "pitch": 0.2527908010330897,
+          "yaw": 0.010995234945132992,
+          "pitch": 0.22302431183819138,
           "rotation": 0.7853981633974483,
           "target": "3-360_dormitorio-1"
         }
@@ -244,16 +244,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.2850053529572598,
-          "pitch": 0.21711298127248035,
-          "rotation": 5.497787143782138,
-          "target": "6-360_bao-dormitorio-individual"
+          "yaw": 1.5676731261170405,
+          "pitch": 0.15610573064313016,
+          "rotation": 0.7853981633974483,
+          "target": "1-360-saln"
         },
         {
-          "yaw": 1.579859977110389,
-          "pitch": 0.18395924598772062,
-          "rotation": 0.7853981633974483,
-          "target": "0-360-saln"
+          "yaw": 1.2844898960280524,
+          "pitch": 0.22136798571515826,
+          "rotation": 4.71238898038469,
+          "target": "6-360_bao-dormitorio-individual"
         }
       ],
       "infoHotspots": []
@@ -282,14 +282,14 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.14069490908934057,
+        "pitch": 0.07008457974132476,
+        "fov": 1.325599857056214
       },
       "linkHotspots": [
         {
-          "yaw": -0.3828802007381569,
-          "pitch": 0.2866557355365895,
+          "yaw": -0.45882982307176867,
+          "pitch": 0.2323082862366661,
           "rotation": 0,
           "target": "5-360-dormitorio-2"
         }
